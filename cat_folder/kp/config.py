12b56346659a6f7487cea9bac313ed0d864c6e31
LeeYinWei <waynelee.ee11@nycu.edu.tw>
2025-12-26 19:12:56 +0800

    kp info

--- cat_folder/kp/config.py
+++ cat_folder/kp/config.py
@@ -1,22 +1,22 @@
 cat_config = {
     "id":101,
-    "hp": 600,
+    "hp": 100000,
     "atk": 400,
     "speed": 4,
     "color": (0, 255, 255),
-    "attack_range": 710,
+    "attack_range": 210,
     "is_aoe": True,
     "kb_limit": 1,
-    "cooldown": 50,
+    "cooldown": 500000,
     "cost": 80,
-    "width":400,
-    "height": 400,
+    "width":200,
+    "height": 200,
     "windup_duration": 200,
     "attack_duration": 100,
     "recovery_duration": 200,
-    "attack_interval": 3000,
-    "idle_frames": ["cat_folder/kp/walking/Screenshot from 2025-12-26 17-03-40.png"],#900
-    "move_frames": [f"cat_folder/kp/walking/Screenshot from 2025-12-26 17-03-40.png"],
+    "attack_interval": 10000,
+    "idle_frames": ["cat_folder/kp/walking/urudcd8G.png"],#900
+    "move_frames": [f"cat_folder/kp/walking/urudcd8G.png"],
     
     "windup_frames": [f"cat_folder/kp/attacking/frame_0000{i}.png" for i in range(85, 95)] \
         ,
@@ -26,10 +26,10 @@ cat_config = {
     
     "kb_frames": [],
     "delta_y": -42, # 微調 y 座標
-    "ibtn_idle": "cat_folder/eraser/ibtn_idle.png",
-    "ibtn_hover": "ccat_folder/eraser/ibtn_hover.png",
-    "ibtn_pressed": "cat_folder/eraser/ibtn_pressed.png",
+    "ibtn_idle": "cat_folder/kp/ibtn_idle.png",
+    "ibtn_hover": "ccat_folder/kp/ibtn_hover.png",
+    "ibtn_pressed": "cat_folder/kp/ibtn_pressed.png",
     "attack_type": "physic",  # 攻擊類型
     "attack_type": "physic",  # 攻擊類型
-    "cat_image": "cat_folder/eraser/cat_image.png",  # 放在levelselection的貓咪圖片
+    "cat_image": "cat_folder/kp/cat_image.png",  # 放在levelselection的貓咪圖片
 }
